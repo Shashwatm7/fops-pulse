2566a5bbd27a37c3cdacb58e463656abab93b366
Shashwat Malik <shashwatmalik@Shashwats-MacBook-Air.local>
2026-06-23 23:35:32 +0530

    Fix migration script to include new tables and auto-run Python seeder on start

--- migrate.js
+++ migrate.js
@@ -14,7 +14,15 @@ async function runMigrations() {
   console.log('Running database migrations...');
   const client = await pool.connect();
   try {
-    const migrationFiles = ['migrations/001_init.sql', 'migrations/002_add_feedback.sql'];
+    const migrationFiles = [
+      'migrations/001_init.sql', 
+      'migrations/002_add_feedback.sql',
+      'migrations/003_baseline_schema.sql',
+      'migrations/004_raw_signals.sql',
+      'migrations/005_canonical_signals.sql',
+      'migrations/006_forecast_outputs.sql',
+      'migrations/007_recommendations.sql'
+    ];
     for (const file of migrationFiles) {
       if (fs.existsSync(file)) {
         console.log(`Executing ${file}...`);
